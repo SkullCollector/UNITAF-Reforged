53570486d72f4324b50d5477cfabd1510c623418
SkullCollector <skull_collector@hotmail.de>
2025-12-16 20:51:04 +0100

    sync laptop with workstation after hardly being productive

--- Scripts/Game/Server/UTF_ServerORBATManager.c
+++ Scripts/Game/Server/UTF_ServerORBATManager.c
@@ -1,32 +1,87 @@
-class SlotData
+class SlotData : JsonApiStruct
 {
 	string playerGameID;
 	string playerUTFName;
 	string callSign;
-	//string role;
-	//string loadoutID;
+	string role;
+	string loadoutID;
+
+	void SlotData()
+    {
+		RegV("playerGameID");
+		RegV("playerUTFName");
+		RegV("callSign");
+		RegV("role");
+		RegV("loadoutID");
+    }
 }
 
-class ORBATData
+class CallSignData : JsonApiStruct
+{
+	string callSign;
+	double primaryFrequency;
+	double secondaryFrequency;
+	double pogoFrequency;
+	array<ref CallSignData> subordinateCallsigns;
+
+	void CallSignData()
+	{
+		RegV("callSign");
+		RegV("subordinateCallsigns");
+		RegV("primaryFrequency");
+		RegV("secondaryFrequency");
+		RegV("pogoFrequency");
+	}
+}
+
+class Metadata : JsonApiStruct
+{
+	string missionName;
+	int slotsCount;
+	string description;
+	string intelligenceOfficer;
+	string fieldLeader;
+
+	void Metadata()
+	{
+		RegV("missionName");
+		RegV("slotsCount");
+		RegV("description");
+		RegV("intelligenceOfficer");
+		RegV("fieldLeader");
+	}
+}
+
+class ORBATData : JsonApiStruct
 {
 	// @TODO: decide if ORBAT ID needs to be string
 	// "felt" like it when concatenating URLs but unsure
 	string orbatID;
+	Metadata metadata;
 	array<ref SlotData> slots;
-	ref map metadata;
+	array<ref CallSignData> callSigns;
+	
+	void ORBATData()
+	{
+		RegV("orbatID");
+		RegV("metadata");
+		RegV("slots");
+		RegV("callSigns");
+	}
 }
+
 
 class ServerORBATManager
 {
 	ref UTF_HTTPService m_http;
 	ref map<string, ref ORBATData> m_cache;
-	
+
 	void ServerORBATManager(UTF_HTTPService http)
 	{
 		m_http = http;
 		m_cache = new map<string, ref ORBATData>;
 	}
-	
+
 	// Fetch ORBAT data from remote service
 	// @TODO: 	separating individual slot fetches after initial ORBAT structure
 	// 			to reduce JSON container size (because of large loadout data)
@@ -37,33 +92,32 @@ class ServerORBATManager
 		ORBATGetCallback cb = new ORBATGetCallback(this, orbatID);
 		m_http.ExecuteGet(url, cb);
 	}
-	
+
 	void OnOrbatJson(string orbatID, string json)
 	{
 		SCR_JsonLoadContext ctx = new SCR_JsonLoadContext;
 		ctx.LoadString(json);
-		
+
 		ORBATData data = ParseORBAT(ctx);
 		data.orbatID = orbatID;
 		m_cache.Set(orbatID, data);
-		
-		// @TODO: memoise data and separate this call?
-		// ApplyORBATtoPlayer(data);
 	}
-	
+
 	ORBATData ParseORBAT(SCR_JsonLoadContext ctx)
 	{
 		ORBATData data = new ORBATData();
-		data.slots = new array <ref SlotData>();
-		data.metadata = new map();
-		
+		data.orbatID = ctx.ReadString("orbatID");
+		data.slots = new array<ref SlotData>();
+		data.callSigns = new array<ref CallSignData>();
+		data.metadata = new Metadata();
+
 		// Handle metadata
 		// @TODO: figure out what metadata to include from endpoint
 		string name;
 		ctx.ReadString("name", name);
 		data.metadata.Set("name", name);
 		data.metadata.Set("slotsCount", slotsCount);
-		
+
 		// Handle slots
 		// @TODO: Handle slots
 		for (int i = 0; i < metadata.slotsCount; i++) {
@@ -74,14 +128,15 @@ class ServerORBATManager
 			slot.callSign = slotCtx.ReadString("callSign");
 			data.slots.Insert(slot);
 		}
-		
-		
+
 	}
-	
-	void ApplyORBATtoPlayer(PlayerController pc)
+
+	void ApplyORBATtoPlayer(int playerID)
 	{
 		// @TODO: implement player ORBAT assignment
-	}	
+		// mostly pseudocode:
+		string playerGameID = BackendApi.GetPlayerIdentityId(playerID);
+	}
 
 	void ApplyORBATtoAll(ORBATData data, bool nearRPrequired = true)
 	{
